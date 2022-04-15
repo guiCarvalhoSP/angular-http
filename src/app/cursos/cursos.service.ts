2a9ece327a651c949d557aeac737042f6be88245
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2022-04-15 11:11:08 -0300

    Correção + pipe-async

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { delay } from 'rxjs/operators'
 import { ICurso } from './icurso';
 import { environment } from '../../environments/environment';
 
@@ -8,10 +9,13 @@ import { environment } from '../../environments/environment';
 })
 export class CursosService {
 
-  private readonly API = `${environment.API}cursos`;
+  private readonly API = `${environment.API}`;
   constructor(private http: HttpClient) { }
 
   list() {
-    return this.http.get<ICurso[]>(this.API);
+    return this.http.get<ICurso[]>(this.API)
+    .pipe(
+      delay(2000)
+    );
   }
 }
